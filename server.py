62e36bfa8d33c4740e20103cb004a8bc8aad9e91
muzy06038 <sub06038@naver.com>
2022-02-03 12:18:52 +0000

    grpc base

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import grpc
-import concurrent from futures # 비동기
+from concurrent import futures # 비동기
 
 import hello_grpc_pb2 # message class
 import hello_grpc_pb2_grpc # client & server class
@@ -12,5 +12,20 @@ class MyServiceServicer(hello_grpc_pb2_grpc.MyServiceServicer):
     def MyFunction(self, request, context):
 
         response = hello_grpc_pb2.MyNumber()
-        
 
+        response.value = hello_grpc.my_func(request.value)
+
+        return response
+
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+hello_grpc_pb2_grpc.add_MyServiceServicer_to_server(MyServiceServicer(), server)
+
+print('Starting server, Listening on port 50051')
+server.add_insecure_port('[::]:50051')
+server.start()
+
+try:
+    server.wait_for_termination()
+except KeyboardInterrupt:
+    server.stop(0)
